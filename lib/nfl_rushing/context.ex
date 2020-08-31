026d561d93b62dd411fd33e4cdfb4fccd137fe7e
Dylan Kendal <dylankendal@gmail.com>
2020-08-31 16:27:55 -0400

    Add very basic column sorting

--- lib/nfl_rushing/context.ex
+++ lib/nfl_rushing/context.ex
@@ -2,7 +2,7 @@ defmodule NflRushing.Context do
   defmacro __using__(_) do
     quote do
       import Ecto, only: [assoc: 2], warn: false
-      import Ecto.Query, only: [from: 2], warn: false
+      import Ecto.Query, only: [from: 1, from: 2], warn: false
       import Ecto.Changeset, warn: false
 
       alias Ecto.Changeset, warn: false
--- lib/nfl_rushing_web/live/rush_live/index.ex
+++ lib/nfl_rushing_web/live/rush_live/index.ex
@@ -4,12 +4,21 @@ defmodule NflRushingWeb.RushLive.Index do
   alias NflRushing.Stats
   alias NflRushing.Stats.Rush
 
+  defstruct []
+
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, :rushes, list_rushes())}
+  def mount(params, _session, socket) do
+    {:ok, assign(socket, :rushes, list_rushes(params))}
   end
 
   @impl true
+  def handle_params(%{"sort_by" => _, "dir" => _} = params, _url, socket) do
+    {:noreply,
+     socket
+     |> assign(:rushes, list_rushes(params))
+     |> apply_action(socket.assigns.live_action, params)}
+  end
+
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
@@ -40,7 +49,20 @@ defmodule NflRushingWeb.RushLive.Index do
     {:noreply, assign(socket, :rushes, list_rushes())}
   end
 
-  defp list_rushes do
+  defp list_rushes(%{"sort_by" => sort_field, "dir" => dir}) do
+    use NflRushing.Context
+
+    from(r in Rush, order_by: [asc: ^String.to_existing_atom(sort_field)])
+    |> Repo.all()
+  end
+
+  defp list_rushes() do
     Stats.list_rushes()
+  end
+
+  # Templating
+
+  def sort_link(socket, text, field) do
+    live_patch(text, to: Routes.rush_index_path(socket, :index, sort_by: field, dir: :asc))
   end
 end
