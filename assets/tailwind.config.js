6d8ed40d356c94739571a8156c58080db6bc772c
Dylan Kendal <dylankendal@gmail.com>
2020-09-01 10:54:13 -0400

    Install tailwind and postcss

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
